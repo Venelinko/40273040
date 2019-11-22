7e3c860df60616ad38fb9c3f9d32d1e6adba07c4
Venelinko <venelin.33@abv.bg>
2019-11-22 16:56:43 +0000

    initial database added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 main = Flask(__name__)
 main.config.from_object(Config)
+db = SQLAlchemy(main)
+migrate = Migrate(main, db)
 
-from app import routes
+from app import routes, models
