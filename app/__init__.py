e84d46df2e681012e4afc015482600ee5809c21b
Venelinko <venelin.33@abv.bg>
2019-11-29 15:21:16 +0000

    database and logging in + registering

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 main = Flask(__name__)
 main.config.from_object(Config)
 db = SQLAlchemy(main)
 migrate = Migrate(main, db)
+login = LoginManager(main)
+login.login_view = 'login'
 
 from app import routes, models
--- climber.py
+++ climber.py
@@ -1,1 +1,6 @@
-from app import main
+from app import main, db
+from app.models import User, Article
+
+@main.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Article': Article }
