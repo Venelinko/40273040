a703a5e8373782410a64c69116c5cfff80481799
Venelinko <venelin.33@abv.bg>
2019-11-29 15:53:03 +0000

    profile page

--- app/routes.py
+++ app/routes.py
@@ -59,3 +59,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@main.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    atricles = [
+        { 'author': user, 'body': 'Post number 1' },
+        { 'author': user , 'body': 'Post numbr 2'}
+    ]
+    return render_template('user.html', user=user, articles=atricles)
